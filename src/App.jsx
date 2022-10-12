87586353ca1ecd72f79b89453814d6bdd85e26a3
PerezFe <PerezFernandoE83@gmail.com>
2022-10-12 20:17:32 -0300

    agregamos un spinner como componente adicional

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import { Container } from 'react-bootstrap';
 import stilo from "./style.css"
 import {useEffect, useState} from "react"
 import Cardhomero from './components/Cardhomero';
+import Spinner from './components/Spinner';
 
 
 
 function App() {
   const [personaje, setPersonaje] = useState({});
+  const [mostrarSpinner, setMostrarSpinner] = useState(true);
 
   useEffect(()=>{
     consultarAPI();
@@ -18,17 +20,26 @@ function App() {
   const consultarAPI = async()=>{
     
     try {
+        //mostrar el spinner
+        setMostrarSpinner(true)
         const respuesta = await fetch("https://thesimpsonsquoteapi.glitch.me/quotes")
         console.log(respuesta)
         const dato = await respuesta.json();
         console.log(dato[0]);
         setPersonaje(dato[0]);
+        //mostrar el componente frase
+        setMostrarSpinner(false);
         
       } catch (error) {
         console.log(error)
         //mostrar un mensaje al usuario que ocurrio un error
       }
   }
+
+  //condicion logica operador ternario ?
+  // (condicion logica)?(logica a ejecutar si cumplo la condicion):(else)
+  const mostrarComponente = (mostrarSpinner)?(<Spinner></Spinner>):(<Cardhomero personaje={personaje}></Cardhomero>)
+
 
   return (
     
@@ -38,9 +49,10 @@ function App() {
       <div className='text-center my-5'>
       <Button variant="warning" onClick={consultarAPI}>Obtener frase</Button>
       </div>
-      
     </div>
-    <Cardhomero personaje={personaje}></Cardhomero>
+    {
+      mostrarComponente
+    }
     </Container>
   );
 }
